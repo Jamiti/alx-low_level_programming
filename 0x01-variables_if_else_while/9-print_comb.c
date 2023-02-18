b217a03d7fcbf4fbb531751c40515dfdf4432cb7
Jamiti <amiticemary@gmail.com>
2023-02-18 10:34:01 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints  all possible combimations of single-digit numbers
+ * numbers must be separated by , followed by a space
+ * numbers should be printed in ascending order
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int d;
+
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(',');
+}
+}
+putchar('\n');
+
+return (0);
+}
