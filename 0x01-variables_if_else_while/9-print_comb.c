3584b76bb2a57894ca8131bba4401c012a49815f
Jamiti <amiticemary@gmail.com>
2023-02-19 09:02:00 -0800

    Patiance, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,19 +9,18 @@
  */
 
 int main(void)
-
 {
 int d;
 
-for (d = '0'; d <= '9'; d++)
+for (d = 1; d <= 9; d++)
 {
 putchar(d);
+if (d != 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
 putchar('\n');
-
 return (0);
 }
