765c06f3cb606ad17da46921de87042e1fceda0a
Jamiti <amiticemary@gmail.com>
2023-02-25 00:13:10 -0800

    Header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,21 +1,12 @@
-#ifndef MAIN.H
-#define MAIN.H
-
-/**
- * file - main.h
- * Author - Jamiti
- * header contains all prototypes for this project
- *
- */
-
-int _isupper(int c)
-int _isdigit(int c)
-int mul(int a, int b)
-void print_numbers(void)
-void print_most_numbers(void)
-void more_numbers(void)
-void print_line(int n)
-void print_diagonal(int n)
-void print_square(int size)
-void print_triangle(int size)
-void print_number(int n)
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
