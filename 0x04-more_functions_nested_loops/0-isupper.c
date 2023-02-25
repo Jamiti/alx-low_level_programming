b12000abb4208a237d9faf336b0e30943cfe8f05
Jamiti <amiticemary@gmail.com>
2023-02-25 00:34:23 -0800

    Headder file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - checks for uppercase character
+ * Returns 1 if c is uppercase
+ * Returns 0 other wise
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return ('\n')
+}
