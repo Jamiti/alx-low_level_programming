85fccf22ff0de4ef2d50eb72a1b15fa19190fd9d
Jamiti <amiticemary@gmail.com>
2023-02-28 14:19:07 -0800

    winning is only half of it. having fun is the other half

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,15 @@
 
 void rev_string(char *s)
 {
-int count = 0; i = 0;
-char last_char;
+int i, length1, length2;
+char t;
 
-while (*s)
-{
-s++;
-count++;
-}
-for (i = 0; i < count; i++)
-s--;
-for (i = 0; i < (count / 2); i++)
-{
-int last = count - 1;
-last_char = s[last - i];
-s[last - i] = s[i];
-s[i] = last_char;
-}
+length1 = 0;
+length2 = 0;
+
+while (s[length1] != '\0')
+length1++
+
+length2 = length1 - 1
+
 }
